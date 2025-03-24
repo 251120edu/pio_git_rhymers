791dc488104939dc8bbc8acd9ed598d93a11616c
unknown <251120@helium.com>
2025-03-24 10:54:44 +0100

    9. zmiana atrybutow klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
-    public static final int INITIAL = -1;
-    public static final int DEAFULT = -1;
+    private static final int MAX_SIZE = 12;
+    private static final int INITIAL = -1;
+    private static final int DEAFULT = -1;
     private final int[] numbers = new int[MAX_SIZE];
 
     public int total = INITIAL;
@@ -28,7 +28,7 @@ public class DefaultCountingOutRhymer {
         return numbers[total];
     }
 
-    public int countOut() {
+    protected int countOut() {
         if (callCheck())
             return DEAFULT;
         return numbers[total--];

3ef5527e5d7ff0d0cf16a9303b44c29b6689f144
Piotr Augustyniak <251120@edu.p.lodz.pl>
2025-03-31 19:42:31 +0200

    15. dokonanie walidacji testami

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+//    Kod jest poprawny
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
